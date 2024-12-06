122a9348ca36d5c078391f0a24179d69454caa3e
ASUS <hantububu5758@gmail.com>
2024-12-06 17:41:14 +0700

    final

--- app/src/main/java/com/example/room/TambahDaftar.kt
+++ app/src/main/java/com/example/room/TambahDaftar.kt
@@ -2,6 +2,7 @@ package com.example.room
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -25,6 +26,8 @@ class TambahDaftar : AppCompatActivity() {
 
     var tanggal = getCurrentDate()
     var DB = daftarBelanjaDB.getDatabase(this)
+    var iID : Int = 0
+    var iAddEdit : Int = 0
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +39,25 @@ class TambahDaftar : AppCompatActivity() {
         _etJumlah = findViewById(R.id.etJumlah)
         _btnTambah = findViewById(R.id.btnTambah)
         _btnUpdate = findViewById(R.id.btnUpdate)
+
+        iID = intent.getIntExtra("id", 0)
+        iAddEdit = intent.getIntExtra("addEdit", 0)
+
+        if (iAddEdit == 0) {
+            _btnTambah.visibility = View.VISIBLE
+            _btnUpdate.visibility = View.GONE
+            _etItem.isEnabled = true
+        } else {
+            _btnTambah.visibility = View.GONE
+            _btnUpdate.visibility = View.VISIBLE
+            _etItem.isEnabled = false
+
+            CoroutineScope(Dispatchers.IO).async {
+                val item = DB.fundaftarBelanjaDAO().getItem(iID)
+                _etItem.setText(item.item)
+                _etJumlah.setText(item.item)
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.actTambahDaftar)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -51,6 +73,17 @@ class TambahDaftar : AppCompatActivity() {
                         item = _etItem.text.toString(),
                         jumlah = _etJumlah.text.toString()
                     )
+                )
+            }
+        }
+
+        _btnUpdate.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).async {
+                DB.fundaftarBelanjaDAO().update(
+                    isi_tanggal = tanggal,
+                    isi_item = _etItem.text.toString(),
+                    isi_jumlah = _etJumlah.text.toString(),
+                    pilihid = iID
                 )
             }
         }
